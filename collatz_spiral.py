6930d7079ebb8d97c7713c869dea9b0ebbc3dfda
fraserdscott <fraserdscott@gmail.com>
2020-03-18 16:30:21 +0000

    Find up to 3 cycles

--- collatz_spiral.py
+++ collatz_spiral.py
@@ -1,5 +1,10 @@
 import math
 import itertools as it
+import unittest
+from collections import deque
+
+MAX_CONSECUTIVE_DIVISIONS = 20
+MAX_CYCLE_LEN = 100
 
 def collatz(number, a, b):
     if number % c == 0:
@@ -9,8 +14,13 @@ def collatz(number, a, b):
 
 
 def denominator(lengths, a):
-    love = len(lengths)
-    return 2**sum(lengths) - a**love
+    return 2**sum(lengths) - a**len(lengths)
+
+
+def number_of_kinks(cycle):
+    # Find places where value increases (an+b been applied)
+    shifted = list(cycle[1:]) + list([cycle[0]])
+    return len([x for x, y in zip(cycle, shifted) if x < y])
 
 
 def numerator(lengths, a):
@@ -18,13 +28,7 @@ def numerator(lengths, a):
     if love == 0:
         return 1
 
-    return  a*(numerator(lengths[1:], a)) + 2**(sum(lengths))
-
-
-def number_of_kinks(cycle):
-    # Find places where value increases (an+b been applied)
-    shifted = list(cycle[1:]) + list([cycle[0]])
-    return len([x for x, y in zip(cycle, shifted) if x < y])
+    return a*(numerator(lengths[1:], a)) + 2**(sum(lengths))
 
 
 def constraint(t):
@@ -37,41 +41,34 @@ def itery(numbers, num_variables):
 
 
 def number_of_cycles(a, b, kink):
+    if a < 1 and b < 1 and kink < 1:
+        raise("Parameter validation")
+
     # why kink > 1?
     if kink > 1 and a % 3 == 0 and b in {3**i for i in range(1,10)}:
         return 0
     
-    count = 0
-    for t in itery(range(1, MAX_CONSECUTIVE_DIVISIONS), kink): # because x!=y
-        n = b * numerator(t[1:], a) / denominator(t, a)
-        if n > 0 and n.is_integer(): # Each integer solution indicates a different cycle
-            count += 1
-    return count
-
-
-# fix me
-def number_of_3_kink_cycles(a, b):
-    if a % 3 == 0 and b in {3**i for i in range(1,10)}:
-        return 0
-    
+    vectors = [t for t in it.product(range(1, MAX_CONSECUTIVE_DIVISIONS), repeat=kink)]
 
     #make shift solution will check that solution does not match any previous found
-    found_ugh = set()
+    found = set()
     count = 0
-    for x in range(1, MAX_CONSECUTIVE_DIVISIONS+1): 
-        for y in range(1, MAX_CONSECUTIVE_DIVISIONS+1):
-            for z in range(1, y):
-                t = (x,y,z)
-                n = b*numerator(t[1:],a)/denominator(t, a)
-                if n > 0 and n.is_integer():
-                    new_cycle = True
-                    for poo in found_ugh:
-                        shifted = list(poo[1:]) + list([poo[0]])
-                        if t == tuple(shifted):
-                            new_cycle = False
-                    if new_cycle:
-                        found_ugh.add(t)
-                        count += 1
+    for t in vectors:
+        if kink > 1 and len(set(t)) == 1:
+            continue
+        n = b*numerator(t[1:],a)/denominator(t, a)
+        if n > 0 and n.is_integer():
+            new_cycle = True
+            for poo in found:
+                for i in range(kink):
+                    items = deque(poo)
+                    items.rotate(-i)
+                    if t == tuple(items):
+                        new_cycle = False
+                        break
+            if new_cycle:
+                found.add(t)
+                count += 1
     return count
 
 
@@ -102,28 +99,41 @@ def find_cycles(a, b):
         n_checked.add(n)
     return cycles
 
-     
-def test():
-    for a in range(3,100,2):
-        for b in range(1,100,2):
-            cycles = find_cycles(a, b)
 
-            print("a={}, b={}, cycles={}".format(a, b, len(cycles)))
-            predicted_cycles = [number_of_cycles(a, b, 1), number_of_cycles(a, b, 2), number_of_3_kink_cycles(a,b)]
-            for i in range(1,4):
-                divided_cycles = [cycle for cycle in cycles if number_of_kinks(cycle) == i]
-                for s in sorted(divided_cycles, key=len):
-                    print(s)
+def get_cycles(cycles, k):
+    return [cycle for cycle in cycles if number_of_kinks(cycle) == k]
 
-                if len(divided_cycles) != predicted_cycles[i-1]:
-                    raise Exception("Predicted {}, found {}".format(predicted_cycles[i-1], len(divided_cycles)))
 
-            print()
+class MyTest(unittest.TestCase):
+    def test(self):
+        for a in range(3,100,2):
+            for b in range(1,100,2):
+                cycles = find_cycles(a, b)
 
-    
-MAX_CONSECUTIVE_DIVISIONS = 20
-MAX_CYCLE_LEN = 100
+                print("a={}, b={}, cycles={}".format(a, b, len(cycles)))
+                predicted_cycles = [number_of_cycles(a, b, 1), number_of_cycles(a, b, 2), number_of_cycles(a,b, 3)]
+                for i in range(1,4):
+                    divided_cycles = get_cycles(cycles, i)
+                    for s in sorted(divided_cycles, key=len):
+                        print(s)
+
+                    self.assertGreaterEqual(len(divided_cycles), predicted_cycles[i-1], "Predicted more cycles than there was")
+                    self.assertLessEqual(len(divided_cycles), predicted_cycles[i-1], "Found more cycles than predicted")
+
+                print()
+
+
+    def check(self):
+        for a in range(3,50,6):
+            for b in [3**x for x in range(10)]:
+                cycles = find_cycles(a, b)
+
+                for i in range(2,4):
+                    divided_cycles = get_cycles(cycles,i)
+
+                    self.assertEqual(len(divided_cycles), 0, "Found an a and b with more than 0")
+                    
 
 c = 2   # /2
 
-test()
+MyTest().test()
