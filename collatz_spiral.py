6d02617e7d803b9093e1176d4188560f5ec99cde
fraserdscott <fraserdscott@gmail.com>
2020-04-03 10:53:43 +0100

    Stuff

--- collatz_spiral.py
+++ collatz_spiral.py
@@ -44,8 +44,21 @@ def x_values_repeat(t):
     facs = no_factors[len(t)]
     for f in facs:
         if len(t) > f and all(t[i]==t[i+f] for i in range(0, len(t)//f)):
-            return True
-    return False
+            return False
+    return True
+
+
+def lol(t):
+    individual_values = [t[0]] + [j-i for (i,j) in zip(t, t[1:])]
+    return not (len(t) > 1 and len(set(individual_values)) == 1) and x_values_repeat(individual_values)
+
+
+def number_of_1_cycles(a, b, kink=1):
+    if kink != 1:
+        raise("Not implemented")
+
+    return len([fac for fac in factors(b) if math.log2(fac + a).is_integer()])
+            
 
 
 def number_of_cycles(a, b, kink, mins=1, maxs=9999999):
@@ -55,34 +68,41 @@ def number_of_cycles(a, b, kink, mins=1, maxs=9999999):
     if kink > 1 and a % 3 == 0 and b in {3**i for i in range(1,10)}:
         return 0
 
+    #if kink==1:
+        #return number_of_1_cycles(a, b)
+    
     no_fs = no_factors[kink]
 
     # If the users bound is greater than the rigourous bound, use the rigourous bound
-    mins = max(mins, int(math.log2(a))) # There is also a better lower limit on the total but it doesnt work rn
-    maxs = min(maxs, kink*(int(math.log2(a+b))+1))
+    mins = max(mins, int(math.log2(a))) # There is also a better lower limit on the total but it doesnt work rn - bounf on total is at least kink
+    maxs = min(maxs, kink*(math.ceil(math.log2(a+b))))
     
     denoms = {i:denominator(i, a, kink) for i in range(mins,maxs+1)}
 
-    vectors = [t for t in it.product(range(mins,maxs+1), repeat=kink) if all(i < j for i, j in zip(t, t[1:]))]
+    vectors = [t for t in it.product(range(mins,maxs+1), repeat=kink) if all(i < j for i, j in zip(t, t[1:])) and lol(t)]
 
     found = set()
     count = 0
+    #why not have total as it's own loop? might make more sense idk
+    #i guess you could precompute every loop tho
     for t in vectors:
-        individual_values = [t[0]] + [j-i for (i,j) in zip(t, t[1:])]
-        if (len(t) > 1 and len(set(individual_values)) == 1) or x_values_repeat(individual_values):
-            continue
 
+        #ts left to check *kink
         n = b*numerator(t[:-1],a)/denoms[t[-1]]
-        if n > 0:
-            if n.is_integer():
+        if b/denoms[t[-1]] > 0 and (b/denoms[t[-1]]).is_integer():
+            print(t, b/denoms[t[-1]], n)
+        # b/numen * denom is an integer. What does this take? what can numen tell us? basically denom can remove facs from b and bring it into deficit. if numen can take it out of deficit, youre good
+        # this can tell us when we're done!
+        # if its an integer just kill it then!s
+        if n > 0 and n.is_integer():
                 # Convert to tuple to make hashable and preserve order
                 cycle = tuple(find_cycle(a,b,n))
 
                 # Check if this cycle has same elements as found one
-                cycle_already_found = set(cycle) in [set(s) for s in found]
+                cycle_already_found = any(c in found for c in cycle)
 
                 if not cycle_already_found:
-                    found.add(cycle)
+                    found.add(n)
                     count += 1
                     if kink == 1 and count == no_fs:
                         return count
@@ -141,7 +161,7 @@ class MyTest(unittest.TestCase):
     def test(self):
         MAX_A = 200
         MAX_B = 200
-        MAX_KINKS = 4
+        MAX_KINKS = 2
 
         for a in range(3,MAX_A,2):
             for b in range(1,MAX_B,2):
@@ -149,7 +169,7 @@ class MyTest(unittest.TestCase):
 
                 print("a={}, b={}, cycles={}".format(a, b, len(cycles)))
                 for kink in range(1, MAX_KINKS+1):
-                    predicted_cycles = number_of_cycles(a, b, kink, maxs=15)
+                    predicted_cycles = number_of_cycles(a, b, kink, maxs=1500)
                     divided_cycles = get_cycles(cycles, kink)
 
                     print("{} cycles with {} kinks".format(predicted_cycles, kink))
@@ -177,5 +197,3 @@ c = 2   # /2
 
 MyTest().test()
 
-
-print(2-j >= 4/2 )
